a5b766291f6028bf204fd213e9f205aaf1d657bd
htam <anonimozurik@gmail.com>
2019-08-10 11:48:19 -0300

    Updates

---
+++ precache-manifest.f802491420af7d979cf136fbc4d47a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447cdc85cce350f3418af9cdff1bf72d",
+    "url": "/htam00/Calculator/index.html"
+  },
+  {
+    "revision": "260730bc12a86573b847",
+    "url": "/htam00/Calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "65a14d85b32f5e24076f",
+    "url": "/htam00/Calculator/static/js/2.49fb6282.chunk.js"
+  },
+  {
+    "revision": "260730bc12a86573b847",
+    "url": "/htam00/Calculator/static/js/main.3a7f2361.chunk.js"
+  },
+  {
+    "revision": "1daf80e89c29c2230841",
+    "url": "/htam00/Calculator/static/js/runtime~main.018f7ae0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/htam00/Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
